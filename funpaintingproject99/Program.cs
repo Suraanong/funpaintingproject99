ad73c27a42b7fb074cda02197687a40de9fd26b3
4ourroro <sujinthornfour@gmail.com>
2025-11-01 22:11:53 +0700

    Form1

--- funpaintingproject99/Program.cs
+++ funpaintingproject99/Program.cs
@@ -16,7 +16,7 @@ namespace funpaintingproject99
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Neoclassical_main());
+            Application.Run(new Form1());
         }
     }
 }
